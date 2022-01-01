615e6378e46434f156803a859d9e9b9b63a908ba
CompassGenius <96708902+CompassGenius@users.noreply.github.com>
2022-01-01 07:55:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -100,11 +100,6 @@ async def start(bot, cmd):
                 [
                     [
                         InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
-                        InlineKeyboardButton("Other Bots", url="https://t.me/subin_works/122")
-                    ],
-                    [
-                        InlineKeyboardButton("About", callback_data="about")
-                    ]
                 ]
             )
         )
