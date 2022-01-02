f3eb9b278a8381674575baf482ffeca3d5c679fe
CompassGenius <96708902+CompassGenius@users.noreply.github.com>
2022-01-02 11:26:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -82,11 +82,11 @@ async def start(bot, cmd):
         invite_link = await bot.create_chat_invite_link(int(AUTH_CHANNEL))
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="**Please Join My Updates Channel to use this Bot!**",
+            text="**You Need To Join Our Channel and Press Refresh Button to get the file\n🍎ഫയലുകൾ ലഭിക്കുന്നതിനായി നിങ്ങൾ ഞങ്ങളുടെ ചാനലിൽ join ചെയ്യണം\n🍎 ശേഷം refresh button അമർത്തുക**",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
+                        InlineKeyboardButton("Join Channel", url=invite_link.invite_link)
                     ]
                 ]
             )
